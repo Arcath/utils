8fd0b994d56f74139c6659878a56c70a8ab90b7c
Adam Laycock <adam@arcath.net>
2020-03-03 09:59:22 +0000

    add missing global to regex and release 0.0.12

--- src/functions/parameterize.spec.ts
+++ src/functions/parameterize.spec.ts
@@ -3,6 +3,6 @@ import {parameterize} from './parameterize'
 describe("Parameterize", () => {
   it('should make strings safe for urls', () => {
     expect(parameterize("My Article's Title")).toBe('my-articles-title')
-    expect(parameterize("S0m3 Long $tring wITH odd (strange) chars in it")).toBe('s0m3-long-tring-with-odd-(strange)-chars-in-it')
+    expect(parameterize("S0m3 Long $tring wITH odd (strange) chars in it...")).toBe('s0m3-long-tring-with-odd-strange-chars-in-it')
   })
 })
--- src/functions/parameterize.ts
+++ src/functions/parameterize.ts
@@ -1,3 +1,3 @@
 export const parameterize = (slug: string): string => {
-  return slug.toLowerCase().replace(/[^a-zA-Z0-9 -]/, "").replace(/\s/g, "-")
+  return slug.toLowerCase().replace(/[^a-zA-Z0-9 -]/g, "").replace(/\s/g, "-")
 }
