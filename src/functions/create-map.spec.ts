2d418240fcabe313dcae49a155e585d41449d709
Adam Laycock <adam@arcath.net>
2020-12-14 14:28:55 +0000

    add create-map

---
+++ src/functions/create-map.spec.ts
@@ -0,0 +1,23 @@
+import {createMap} from './create-map'
+
+describe('Create Map', () => {
+  it('should create a map', () => {
+    const t1 = createMap(9, 9, false)
+
+    expect(t1.map.length).toBe(10)
+
+    const t2 = createMap(9, 9, false, -9, -9)
+
+    expect(t2.map.length).toBe(19)
+
+    expect(t2.get(-8, 7)).toBe(false)
+
+    t2.set(-8, 7, true)
+
+    expect(t2.get(-8, 7)).toBe(true)
+
+    t2.setRange(2, 4, 2, 4, true)
+
+    expect(t2.get(3,4)).toBe(true)
+  })
+})
---
+++ src/functions/create-map.ts
@@ -0,0 +1,61 @@
+interface Map<T>{
+  /** Get the value at position X,Y */
+  get(x: number, y: number): T
+  /** Set the value of X,Y to `value` */
+  set(x: number, y: number, value: T): void
+  /** Set all the cells in the range to `value` */
+  setRange(minX: number, maxX: number, minY: number, maxY: number, value: T): void
+  /** The underlying map. Be warned this map maybe shifted so that 0 = minX etc... */
+  map: T[][]
+}
+
+/**
+ * Create a co-ordinate grid
+ * 
+ * @param maxX The maximum X value.
+ * @param maxY The maximum Y value.
+ * @param initialValue THe default value for all cells.
+ * @param minX (Optional) The minimum X value, defaults to 0
+ * @param minY (Optional) The minimum Y value, defaults to 0
+ */
+export const createMap = <T>(maxX: number, maxY: number, initialValue: T, minX = 0, minY = 0): Map<T> => {
+  const map: T[][] = []
+
+  const translateX = (x: number) => {
+    return x - minX
+  }
+
+  const translateY = (y: number) => {
+    return y - minY
+  }
+
+  for(let y = minY; y <= maxY; y++){
+    map[translateY(y)] = []
+    for(let x = minX; x <= maxX; x++){
+      map[translateY(y)][translateX(x)] = initialValue
+    }
+  }
+
+  const get = (x: number, y: number) => {
+    return map[translateY(y)][translateX(x)]
+  }
+
+  const set = (x: number, y: number, value: T) => {
+    map[translateY(y)][translateX(x)] = value
+  }
+
+  const setRange = (minX: number, maxX: number, minY: number, maxY: number, value: T) => {
+    for(let y = minY; y <= maxY; y++){
+      for(let x = minX; x <= maxX; x++){
+        set(x, y, value)
+      }
+    }
+  }
+
+  return {
+    get,
+    set,
+    setRange,
+    map
+  }
+}
