233ac1fce39b06830afe77ea68152b8200498791
Adam Laycock <adam@arcath.net>
2021-07-15 17:24:38 +0100

    fix: swap deepIncludes to reducio

--- src/functions/deep-includes.ts
+++ src/functions/deep-includes.ts
@@ -1,4 +1,5 @@
 /* eslint @typescript-eslint/no-explicit-any:off */
+import {reducio} from './reduce'
 
 /**
  * Check if an array exists inside an array
@@ -11,17 +12,9 @@ export const deepIncludesArray = <T extends any[]>(
   array: T[],
   compare: T
 ): boolean => {
-  return array.reduce<boolean>((includes, value) => {
-    if (includes) {
-      return true
-    }
-
-    return value.reduce<boolean>((match, v, i) => {
-      if (!match) {
-        return false
-      }
-
-      return v === compare[i]
-    }, true)
-  }, false)
+  return reducio(array, entry => {
+    return reducio(entry, (v: any, i: number) => v === compare[i], {
+      initial: true
+    })
+  })
 }
