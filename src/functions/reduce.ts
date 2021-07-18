9fe020bfa49ede962b25bd90b25d7a2179a904c8
Adam Laycock <adam@arcath.net>
2021-07-18 21:15:23 +0100

    fix(lint): fix lint issue with function order

--- src/functions/reduce.ts
+++ src/functions/reduce.ts
@@ -1,35 +1,3 @@
-/**
- * Reduce the array into a single boolean value for if the check function returns true.
- *
- * _once the first `false` is encountered `check` is not run again_
- *
- * @param array The array to test
- * @param check The function to check the array entries with
- * @returns `true` if `check` only ever returned `true`, otherwise `false`
- */
-export const reduceTruthy = <T>(
-  array: T[],
-  check: (entry: T, i: number) => boolean
-) => {
-  return reducio(array, check, {initial: true})
-}
-
-/**
- * Reduce the array into a single boolean value for if the check function returns false.
- *
- * _once the first `true` is encountered `check` is not run again_
- *
- * @param array The array to test
- * @param check The function to check the array entries with
- * @returns `true` if `check` only ever returned `false`, otherwise `false`
- */
-export const reduceFalsy = <T>(
-  array: T[],
-  check: (entry: T, i: number) => boolean
-) => {
-  return reducio(array, (v, i) => !check(v, i), {initial: true})
-}
-
 export type RedicioOptions = {
   /**
    * The initial value to check against. If any check function returns a value
@@ -61,4 +29,36 @@ export const reducio = <T>(
 
     return check(value, i)
   }, initial)
+}
+
+/**
+ * Reduce the array into a single boolean value for if the check function returns true.
+ *
+ * _once the first `false` is encountered `check` is not run again_
+ *
+ * @param array The array to test
+ * @param check The function to check the array entries with
+ * @returns `true` if `check` only ever returned `true`, otherwise `false`
+ */
+export const reduceTruthy = <T>(
+  array: T[],
+  check: (entry: T, i: number) => boolean
+) => {
+  return reducio(array, check, {initial: true})
+}
+
+/**
+ * Reduce the array into a single boolean value for if the check function returns false.
+ *
+ * _once the first `true` is encountered `check` is not run again_
+ *
+ * @param array The array to test
+ * @param check The function to check the array entries with
+ * @returns `true` if `check` only ever returned `false`, otherwise `false`
+ */
+export const reduceFalsy = <T>(
+  array: T[],
+  check: (entry: T, i: number) => boolean
+) => {
+  return reducio(array, (v, i) => !check(v, i), {initial: true})
 }
