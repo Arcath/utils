080f67aa488fae94a96826ec1cd64891069c2b17
Adam Laycock <aml@lhs.lancs.sch.uk>
2022-11-15 15:14:50 +0000

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -3,15 +3,15 @@ import configs from 'kcd-scripts/config.js'
 const config = Object.assign(configs.jest, {
   testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true
+      }
+    ]
   },
   collectCoverage: true,
-  extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true
-    }
-  }
+  extensionsToTreatAsEsm: ['.ts']
 })
 
 export default config
