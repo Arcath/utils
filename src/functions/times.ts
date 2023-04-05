15803fcaef7db506092bb7c9ba77d460cc1ba58a
Arcath <adam@arcath.net>
2023-04-05 10:26:37 +0100

    feat: add `numberArray` and use it in `times` and `asyncTimes`

--- src/functions/times.ts
+++ src/functions/times.ts
@@ -1,18 +1,30 @@
 import {asyncMap} from './async-map'
 
 /**
+ * Build an array of the numbers from `start` to `end`
+ *
+ * @param start number to start from
+ * @param end number to end at
+ * @returns an array of numbers
+ */
+export const numberArray = (start: number, end: number) => {
+  const a: number[] = []
+
+  for (let i = start; i <= end; i++) {
+    a.push(i)
+  }
+
+  return a
+}
+
+/**
  * Repeat the given function `number` times
  *
  * @param number The number of times to itterate
  * @param cb The function to run
  */
 export const times = <T>(number: number, cb: (i: number) => T): T[] => {
-  const result: T[] = []
-  for (let i = 1; i <= number; i++) {
-    result.push(cb(i))
-  }
-
-  return result
+  return numberArray(1, number).map(cb)
 }
 
 /**
@@ -25,13 +37,7 @@ export const asyncTimes = async <T>(
   number: number,
   cb: (i: number) => Promise<T>
 ): Promise<T[]> => {
-  const numbers: number[] = []
-
-  for (let i = 1; i <= number; i++) {
-    numbers.push(i)
-  }
-
-  return asyncMap(numbers, cb)
+  return asyncMap(numberArray(1, number), cb)
 }
 
-// {times, asyncTimes}
+// {numberArray, times, asyncTimes}
